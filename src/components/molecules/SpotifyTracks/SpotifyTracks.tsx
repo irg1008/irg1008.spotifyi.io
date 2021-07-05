39e95012de9d91cc03258adad5d62867d20547d5
irg1008 <ivansudevlop@gmail.com>
2021-07-05 16:43:03 +0200

    Play from the song card

--- src/components/molecules/SpotifyTracks/SpotifyTracks.tsx
+++ src/components/molecules/SpotifyTracks/SpotifyTracks.tsx
@@ -4,6 +4,7 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import { useCallback, useEffect, useState } from "react";
 import Loading from "components/atoms/Loading";
 import Image from "next/image";
+import useSpotify from "hooks/useSpotify";
 
 interface CustomVariants extends Variants {
 	hide: Variant;
@@ -53,26 +54,36 @@ interface ISpotifyTracks {
 	tracks: TTrack[];
 }
 
-const SpotifyCard = ({ track }: ISpotifyTrack) => (
-	<Styled.Card variants={card}>
-		<Styled.SongTitle>{track.name}</Styled.SongTitle>
-		<Styled.SongArtist>
-			{track.artists.map((a) => a.name).join(" - ")}
-		</Styled.SongArtist>
-		<Styled.SongImg variants={cardImg}>
-			<a href={track.external_urls.spotify} target="_blank" rel="noreferrer">
-				<Image
-					src={track.album?.images[0].url}
-					alt={track.name}
-					layout="fill"
-				/>
-			</a>
-		</Styled.SongImg>
-		<Styled.Audio controls>
-			<source src={track.preview_url} />
-		</Styled.Audio>
-	</Styled.Card>
-);
+const SpotifyCard = ({ track }: ISpotifyTrack) => {
+	const { spotify, withSpotify } = useSpotify();
+
+	const playSong = async () => {
+		await withSpotify(() => spotify.queue(track.uri));
+		await withSpotify(() => spotify.skipToNext());
+	};
+
+	return (
+		<Styled.Card variants={card}>
+			<Styled.SongTitle>{track.name}</Styled.SongTitle>
+			<Styled.SongArtist>
+				{track.artists.map((a) => a.name).join(" - ")}
+			</Styled.SongArtist>
+			<Styled.SongImg variants={cardImg}>
+				<a href={track.external_urls.spotify} target="_blank" rel="noreferrer">
+					<Image
+						src={track.album?.images[0].url}
+						alt={track.name}
+						layout="fill"
+					/>
+				</a>
+			</Styled.SongImg>
+			<Styled.Audio controls={false}>
+				<source src={track.preview_url} />
+			</Styled.Audio>
+			<button onClick={playSong}>Play</button>
+		</Styled.Card>
+	);
+};
 
 const SpotifyTracks = ({ tracks }: ISpotifyTracks) => {
 	const [offset, setOffset] = useState(0);
