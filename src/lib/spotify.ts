52d72cbc421bdb1bdfac57aabca097af02184516
irg1008 <ivansudevlop@gmail.com>
2021-07-10 02:00:00 +0200

    Removed local storage getter setters

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -23,33 +23,5 @@ const getRedirectUri = () => getUrl({ path: "/auth/" });
 
 const spotifyBaseUri = "https://accounts.spotify.com/";
 
-const setLocalData = (data: ISpotifyTokenResponse) => {
-  const { access_token, refresh_token } = data;
-
-  // Set local session data.
-  localStorage.setItem("access_token", JSON.stringify(access_token));
-  localStorage.setItem("refresh_token", JSON.stringify(refresh_token));
-};
-
-const getLocalData = (): ISpotifyTokenResponse => {
-  // Set local session data.
-  const access_token = JSON.parse(localStorage.getItem("access_token"));
-  const refresh_token = JSON.parse(localStorage.getItem("refresh_token"));
-
-  return { access_token, refresh_token };
-};
-
-const removeLocalData = () => {
-  localStorage.removeItem("access_token");
-  localStorage.removeItem("refresh_token");
-};
-
-export {
-  getUrl,
-  getRedirectUri,
-  spotifyBaseUri,
-  setLocalData,
-  getLocalData,
-  removeLocalData,
-};
+export { getUrl, getRedirectUri, spotifyBaseUri };
 export type { ISpotifyTokenResponse, ISpotifyError };
